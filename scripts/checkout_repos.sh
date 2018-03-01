fb9d81fb2276f792725e439eed99b80ba8ab9490
Wojciech Urbański <wojciech.urbanski@codilime.com>
2018-03-01 14:23:31 -0800

    Add dependency on contrail-zuul-jobs to dev-env

--- scripts/checkout_repos.sh
+++ scripts/checkout_repos.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 mkdir -p /root/src/review.opencontrail.org/Juniper/
 [ -d /root/src/review.opencontrail.org/Juniper/contrail-project-config ] || git clone https://github.com/Juniper/contrail-project-config /root/src/review.opencontrail.org/Juniper/contrail-project-config
+[ -d /root/src/review.opencontrail.org/Juniper/contrail-zuul-jobs ] || git clone https://github.com/Juniper/contrail-zuul-jobs /root/src/review.opencontrail.org/Juniper/contrail-zuul-jobs
