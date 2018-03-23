f985723ebae1b6e8d71cfcfb77b9363cc7fc7829
Wojciech Urbański <wojciech.urbanski@codilime.com>
2018-03-23 13:48:03 +0100

    Enable building of single containers
    Change-Id: I8c79a8dfd2462667faf695aaf697f09438a60ad4
    Closes-Bug: 1758953

---
+++ scripts/prepare-containers.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+REPODIR=/root/src/review.opencontrail.org/Juniper/contrail-container-builder
+
+[ -d ${REPODIR} ] || git clone https://github.com/Juniper/contrail-container-builder  ${REPODIR}
+cp tpc.repo.template common.env ${REPODIR}
