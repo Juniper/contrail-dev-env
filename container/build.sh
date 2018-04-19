af16d22f09dda446707a7a0683682cde8eb9425f
Andrei Bunghez <abunghez@lenovo.com>
2018-04-19 16:34:18 +0300

    Add posibility to init repo from a specific branch
    Adding a BRANCH build-arg in order to be able to initilize sandbox from a particular contrail-vnc branch.
    
    usage example:
    docker build --build-arg BRANCH=R5.0 .
    
    Change-Id: I68e52fd400afa03806728575eaba08ab7c916e7e
    Depends-On: I4de919570027ddea6edd20551916a9b3ea872266
    Closes-Bug: #1765408

--- container/build.sh
+++ container/build.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+BRANCH=master
+
+while getopts ":b:" opt; do
+    case $opt in
+      b) BRANCH=$OPTARG
+         ;;
+      \?) echo "Invalid option: $opt"; exit 1;;
+    esac
+done
+
+shift $((OPTIND-1))
+
 TAG=${1:-centos-7.4}
 echo Building contrail-dev-env image: opencontrail/developer-sandbox:${TAG}
-docker build --no-cache --tag opencontrail/developer-sandbox:${TAG} .
+docker build --build-arg BRANCH=${BRANCH} --no-cache --tag opencontrail/developer-sandbox:${TAG} .
