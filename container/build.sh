6e35d2cb35a423912f43cb4bbf13b1c356bb6ad4
Mihai-Costin Broc <mbroc@lenovo.com>
2018-07-18 15:28:10 +0300

    Fix for building from r5.0 tag
    Cloned contrail-container-builder project from r5.0 tag.
    Add a yum repo file for building the sandbox container
    using CentOS-7.4.1708 repositories, because by default
    it would upgrade Centos 7.5 which is not supported by r5.0.
    Use the r5.0 tagged manifest from contrail-vnc when creating
    the sandbox container.
    Use the tag centos-7.4-r5.0 for the sandbox container image
    from the opencontrail docker hub.
    
    After submitting this, need to tag this commit with r5.0 and then
    undo these changes in another commit so that the last version
    on the R5.0 branch should do the build from the latest R5.0
    
    Change-Id: I24472621ee1b8439e4bbc9c1ddfb0d732401deca
    Depends-On: I0964311d7a9eb66d8ab68580b5c867cfb0b3427d
    Partial-Bug: #1782196

--- container/build.sh
+++ container/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-TAG=${1:-centos-7.4}
+TAG=${1:-centos-7.4-r5.0}
 echo Building contrail-dev-env image: opencontrail/developer-sandbox:${TAG}
 docker build --no-cache --tag opencontrail/developer-sandbox:${TAG} .
