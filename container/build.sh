f1d67441712440851519a0c47d005eea5ac238b8
Wojciech Urbański <wojciech.urbanski@codilime.com>
2018-07-24 15:11:44 +0200

    Add publish-dev-env job
    This job publishes dev-env to a specified registry.
    The container is built based on specific sandbox_branch.
    
    Change-Id: I2684570d4b50d795fb586283bb99812038d103b0
    Partial-Bug: #1783332

--- container/build.sh
+++ container/build.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 BRANCH=master
+IMAGE=opencontrail/developer-sandbox
 
-while getopts ":b:" opt; do
+while getopts ":b:i:" opt; do
     case $opt in
       b) BRANCH=$OPTARG
+         ;;
+      i) IMAGE=$OPTARG
          ;;
       \?) echo "Invalid option: $opt"; exit 1;;
     esac
@@ -12,5 +15,5 @@ done
 shift $((OPTIND-1))
 
 TAG=${1:-centos-7.4}
-echo Building contrail-dev-env image: opencontrail/developer-sandbox:${TAG}
+echo Building contrail-dev-env image: ${IMAGE}:${TAG}
 docker build --build-arg BRANCH=${BRANCH} --no-cache --tag opencontrail/developer-sandbox:${TAG} .
