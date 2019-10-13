885ba89d6105f5792c47592a8b24a21d8b15601b
Ananth Suryanarayana <anantha@juniper.net>
2019-10-13 11:33:43 -0700

    Add script to build just the vrouter
    For RHEL DCI, we could use this as often as needed to find potential
    breakages sooner than later.
    
    This only takes around 3 minutes to complete!
    
    Partial-Jira-Bug: CEM-9673
    Change-Id: Ib236ccd9d76001e3ce078a2754c4579458b245b1

---
+++ build_vrouter.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+
+set -ex
+
+TOP="/root/build_vrouter"
+TOPV="$TOP/vrouter"
+PACKAGES="kernel-devel-$(uname -r) autoconf automake libtool flex bison gcc-c++ boost-devel"
+
+function setup () {
+    yum -y update
+    yum -y install $PACKAGES
+    curl -s "https://rpmfind.net/linux/epel/7/x86_64/Packages/p/python2-pip-8.1.2-10.el7.noarch.rpm" > pip.rpm
+    yum -y install pip.rpm || true
+    rm -rf pip.rpm
+    pip install scons
+}
+
+function teardown () {
+    yum -y remove $PACKAGES
+    pip uninstall scons
+}
+
+function update_manifest () {
+    cat > $TOPV/.repo/manifest.xml<<EOF
+<manifest>
+<remote name="github" fetch="https://github.com/Juniper"/>
+<default revision="refs/heads/master" remote="github"/>
+<project name="contrail-build" remote="github" path="tools/build">
+  <copyfile src="SConstruct" dest="SConstruct"/>
+</project>
+<project name="contrail-vrouter" remote="github" path="vrouter"/>
+<project name="contrail-sandesh" remote="github" path="tools/sandesh"/>
+<project name="contrail-common" remote="github" path="src/contrail-common"/>
+</manifest>
+EOF
+}
+
+function checkout () {
+    rm -rf $TOP
+    mkdir -p $TOPV
+    curl https://storage.googleapis.com/git-repo-downloads/repo > $TOP/repo
+    chmod +x $TOP/repo
+    cd $TOPV
+    $TOP/repo init --quiet -u https://github.com/Juniper/contrail-vnc.git
+    update_manifest
+    $TOP/repo sync
+}
+
+function build () {
+    scons -uj8 -C $TOPV build-kmodule
+}
+
+function verify () {
+    insmod $TOPV/vrouter/vrouter.ko
+    lsmod | grep vrouter | grep -v grep
+    rmmod vrouter
+    rm -rf $TOP
+}
+
+function main () {
+    setup
+    checkout
+    build
+    verify
+#   teardown
+}
+
+main
