80a0b0d3312a696d34c83acc7f09d231722cbd5c
Raviteja Gettam <ravitejag>
2020-08-25 15:07:18 -0700

    Changed tpc repository location and added fix for tungstenfabric migration changes
    Closes-Jira-Bug: CE-7236
    
    Depends-On: I714ef28a97274a576f3ef66b33257f07535da16b
    Change-Id: Ib87b5d6fc24bb6da2f24e21c3270aca786334647
    (cherry picked from commit 385d028e5b33ae291d0a240fb466406039c7c4d2)

--- scripts/build-deployers.sh
+++ scripts/build-deployers.sh
@@ -2,6 +2,6 @@
 
 REPODIR=/root/src/review.opencontrail.org/Juniper/contrail-deployers-containers
 
-[ -d ${REPODIR} ] || git clone https://github.com/Juniper/contrail-deployers-containers  ${REPODIR}
+[ -d ${REPODIR} ] || git clone https://github.com/tungstenfabric/tf-deployers-containers  ${REPODIR}
 cp tpc.repo.template common.env ${REPODIR}
 cd ${REPODIR}/containers && ./build.sh
